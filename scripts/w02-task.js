de549f7014a93c9f178954ce775b5899d7488fc4
sterlingwebsite <smsteele14@gmail.com>
2023-09-21 21:33:10 -0600

    finished assignments for w02 learning activity

---
+++ scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Sterling Steele';
+let currentYear = 'currentYear';
+let profilePicture = 'images/IMG_0520.jpeg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.querySelector('img');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear
+imageElement.setAttribute('src', `${profilePicture}`);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+favoriteFoods = ['sushi', 'fish', 'spaghetti', 'ham and cheese sandwich', 'broccoli and cheese soup', 'salmon', 'fruit salad'];
+foodElement.innerHTML = `${favoriteFoods}`;
+let favoriteFood = 'sharp cheddar cheese';
+favoriteFoods.push(`${favoriteFood}`);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
